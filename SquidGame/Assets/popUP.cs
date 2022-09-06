2fc3dd0aa3f3605b121428a810601271fa9a872c
Jai Shree Jain <vijayshrijain2003@gmail.com>
2022-09-07 00:23:01 +0530

    Pop_Up Added

---
+++ SquidGame/Assets/popUP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class popUP : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public GameObject  question;
+        // Start is called before the first frame update
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                question.SetActive(true);
+            }
+        }
+
+
+        //      void OnTriggerExit(Collider other)
+        // {
+        //     if (other.CompareTag("Player"))
+        //     {
+        //         question.enabled = false;
+              
+        //     }
+        // }
+
+       
+
+
+
+       
+        
+    }
+
