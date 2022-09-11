e4dcd70a8b372524fd0491c593ecbd193436fa1c
Shubhi Yadav <shubhi.yadav.1234@gmail.com>
2022-09-11 23:24:18 +0530

    Pop up script added

---
+++ The-Bridge-of-Life-976077a4057d5ea3bf31158597306fef023523b5/SquidGame/Assets/Scripts/LifeManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LifeManager : MonoBehaviour
+{
+    public TMP_InputField answer;
+    public string actualAns;
+    public GameObject glass;
+    //public MouseLook mouse;
+    public GameObject questionImg;
+    //public AudioSource Door1_3;
+    //public AudioSource Correct;
+    //public AudioSource Incorrect;
+    private int scoreCount=0;
+    private int lifeRemaining=3;
+    public Text scoreText;
+    //public Text winText;
+    [SerializeField] private List<Image> lifeImageList;
+   // public string strength;
+    public bool strong=true;
+
+    string input;
+
+    public void startGame(){
+        scoreCount=0;
+        lifeRemaining=3;
+        //winText.text="";
+    }
+     public void reduceLife(int index){
+        lifeImageList[index].enabled=false;
+    }
+    public void onSubmit()
+    {
+        input = answer.text.ToString(); 
+        if(strong){
+            if (input.ToUpper() == actualAns.ToUpper())
+            {
+                //Correct.Play();
+                scoreCount+=20;
+                scoreText.text="Score"+scoreCount;
+                //door.SetActive(false);
+                questionImg.SetActive(false);
+                Debug.Log("You did it!");
+                // MyAnimationController.SetBool("open", true);
+                //Cursor.lockState = CursorLockMode.Locked;
+                //Cursor.visible = false;
+                //mouse.enabled = true;
+                //Door1_3.Play();
+            }
+            else
+            {
+                
+                Debug.Log(input.ToUpper());
+                Debug.Log(actualAns.ToUpper());
+                //Incorrect.Play();
+                /*if(lifeRemaining<=0){
+                    winText.text="Game Over!!";
+                }*/
+            }
+        }
+        else{
+            if (input.ToUpper() == actualAns.ToUpper())
+            {
+                //Correct.Play();
+                //scoreCount+=20;
+                //scoreText.text="Score"+scoreCount;
+                //glass.SetActive(false);
+                questionImg.SetActive(false);
+                Debug.Log("You did it!");
+                // MyAnimationController.SetBool("open", true);
+                //Cursor.lockState = CursorLockMode.Locked;
+                //Cursor.visible = false;
+                //mouse.enabled = true;
+                //Door1_3.Play();
+            }
+            else
+            {
+
+                lifeRemaining--;
+                reduceLife(lifeRemaining);
+                Debug.Log(input.ToUpper());
+                Debug.Log(actualAns.ToUpper());
+                //Incorrect.Play();
+                if(lifeRemaining<=0){
+                  //  winText.text="Game Over!!";
+                    glass.SetActive(false);
+                }
+            }
+        }
+    }
+
+}
+
---
+++ The-Bridge-of-Life-976077a4057d5ea3bf31158597306fef023523b5/SquidGame/Assets/Scripts/PopUpOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpOnTrigger : MonoBehaviour
+{
+    public GameObject questionImg;
+         // Start is called before the first frame update
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                questionImg.SetActive(true);
+            }
+        }
+
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                questionImg.SetActive(false);
+               
+            }
+        }
+}
